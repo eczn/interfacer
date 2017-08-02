aed9c1fd8f1c041a3f6041c29d167a4b96455d86
eczn <1975845422@qq.com>
2017-08-03 00:07:52 +0800

    样式

--- template/js/main.js
+++ template/js/main.js
@@ -1,31 +1,9 @@
 // main.js
-
 document.addEventListener('DOMContentLoaded', function(){
 	FastClick.attach(document.body);
 	console.info('Fastclick Ready');
 }, true);
 
-function tree(root, pathAcc){
-	console.log(root); 
-
-	var list = Object.keys(root); 
-
-	return list.map(sub => {
-		// next 
-		if (typeof root[sub] === 'object') {
-			var nextPath = pathAcc + '/' + sub; 
-			return `
-				<ul class="file-tree" where="${sub}">
-					<a class="header" href="${ nextPath }">${ nextPath }</a>
-					${tree(root[sub], nextPath)}
-				</ul>
-			`; 
-		} else {
-			return ''; 
-		}
-	}).join(''); 
-}
-
-var res = tree(root, ''); 
-
-$('.file-tree-root').html(res); 
+$('.show-file-tree').click(function(){
+	$('body').attr('side-open', this.checked ? '√' : '×'); 
+}); 
