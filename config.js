7088d23b52bc2cd42b22bbacf3b538a39e7dd303
eczn <1975845422@qq.com>
2017-08-03 15:05:46 +0800

    忽略文件、config选项、package.json更新

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 module.exports = {
 	BASE: __dirname, 
-	docRoot: path.join(__dirname, './doc'), 
+	docRoot: path.join(__dirname, './docs'), 
 	output: path.join(__dirname, './output'),
 	PORT: 3322, 
 	docName: 'Interfacer 使用手册'
