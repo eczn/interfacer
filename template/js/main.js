686f7c84ba0ba2aa0e41deeb476630bd64563edd
eczn <1975845422@qq.com>
2017-08-03 10:48:01 +0800

    样式

--- template/js/main.js
+++ template/js/main.js
@@ -1,9 +1,37 @@
 // main.js
-document.addEventListener('DOMContentLoaded', function(){
-	FastClick.attach(document.body);
-	console.info('Fastclick Ready');
-}, true);
+function scrollHandle(){
+	var rate = getRate(); 
+	var rateStr = rate * 100 + '%'; 
+	// console.log(rate, rateStr)
+	$('.top').css('width', rateStr); 
+}
 
-$('.show-file-tree').click(function(){
-	$('body').attr('side-open', this.checked ? '√' : '×'); 
-}); 
+function getRate(){
+	var $b = $('body'); 
+
+	var cha = parseInt($b.css('margin-top')) + parseInt($b.css('margin-bottom'));
+
+	var total = $b.height() + cha - window.innerHeight; 
+	var now = $b.scrollTop(); 
+	// console.log('now, total', now, total); 
+	// console.log('rate:', now / total); 
+	if (total <= 0){
+		return 1; 
+	} else {
+		return now / total; 	
+	}
+}
+
+window.onload = function(){
+	document.addEventListener('DOMContentLoaded', function(){
+		FastClick.attach(document.body);
+		console.info('Fastclick Ready');
+	}, true);
+
+	$('.show-file-tree').click(function(){
+		$('body').attr('side-open', this.checked ? '√' : '×'); 
+	}); 
+
+	$(window).scroll(scrollHandle); 
+	scrollHandle(); 
+}
